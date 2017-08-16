96ffa6b0060aa97de8c2bc006ed0d994efb19318
Brett <Brett@10.0.0.2>
2017-08-15 20:30:12 -0500

    Removing old BO and VO objects, replacing with new VO

---
+++ TwitterMapWebApp/src/view/ChartView.java
@@ -0,0 +1,22 @@
+package view;
+
+public class ChartView {
+	private Object[][] areaChart;
+	private String color;
+
+	public Object[][] getAreaChart() {
+		return areaChart;
+	}
+
+	public void setAreaChart(Object[][] areaChart) {
+		this.areaChart = areaChart;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
